e3ef52ec8d39fd4b17e3e21cad377885c3602c12
LDsAragon <matisalomone@hotmail.com>
2020-01-27 23:20:01 -0300

    Commit: Creando el Controller de Cliente

---
+++ src/main/java/com/maty/models/controller/ClienteRestController.java
@@ -0,0 +1,26 @@
+package com.maty.models.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maty.models.entities.Cliente;
+import com.maty.models.service.IClienteService;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService; 
+	
+	
+	@GetMapping("/clientes")
+	public List<Cliente> findAll(){
+		return clienteService.findAll();
+	}
+	
+}
